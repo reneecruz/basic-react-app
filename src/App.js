4f10f22a800c10b55bd8100a6f067272adb984d7
Renée Cruz <reneebasscruz@gmail.com>
2019-10-28 23:15:35 -0400

    added components, css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Tweet from './Tweet'
 function App () {
 
   return (
-    <div>
-      <h1>This is the App Component</h1>
-      <Tweet />
+    <div className="app">
+      <Tweet name="Beyonce" message="Ladies, get in formation." likes="5K"/>
+      <Tweet name="Coffee Dad" message="Coffee is good today." likes="43"/>
+      <Tweet name="Ye" message="I need that horse." likes="1K"/>
+      <Tweet name="DHH" message="Rework." likes="10K"/>
+      <Tweet name="Mother Theresa" message="I love you." likes="222"/>
+      <Tweet name="Tetragrammaton" message="I love you <3." likes="72K"/>
     </div>
   );
 }
